097075a8bb87f866120d0058baac499173222fc4
Juanpi <juanpigallardo007@hotmail.com>
2019-06-24 23:49:23 -0300

    agrego 3 archivos

---
+++ ejer1.js
@@ -0,0 +1,7 @@
+for(var i=0; i < 31; i ++){
+    if (i % 2 == 0){
+        console.log("Es par :",i);
+    }else{
+        console.log("Es impar: ",i);
+    }
+}
---
+++ ejercicio.js
@@ -0,0 +1,35 @@
+//const valorInicial =  5;
+
+//if(valorInicial == 5 ){
+//    console.log("SIII ES 55")
+//} else {
+//    console.log("Cualquie cosa")
+//}
+
+
+//function nombreCompleto(nombre, apellido){
+//    console.log(nombre + " " + apellido);
+//}
+//nombreCompleto("Sergio","Lara");
+
+//function nombreCompletoConRetorno(nombre,apellido){
+//    return nombre   +   " " +apellido;
+//}
+//var nombre = "Juanpi";
+//let nombreCompleto=nombreCompletoConRetorno(nombre,"jara");
+//console.log(nombreCompleto);
+
+var frutas = ["Manzana","Banana"];
+// console.log(frutas);
+// frutas.push("Pera");
+// console.log(frutas);
+// console.log("La primer fruta es: ",frutas[0]);
+// frutas.push("Naranja",2);
+// console.log(frutas);
+
+for(var fruta in frutas){
+    console.log("nombre de la fruta: ",frutas[fruta]);
+}
+frutas.forEach(fruta => [
+    console.log(fruta)
+])
